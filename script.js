6df7830184026f7e776ca181d3ec891f88d3527b
Davis <davismickens98@gmail.com>
2024-02-28 17:28:34 -0500

    index.html: defered script,js; script.js: added game controller IIFE, gameboard IIFE, and players factory

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-const gameboard = (function () {
-    let board = 
-    [
-        ['X','',''],
-        ['','X',''],
-        ['','','X']
-    ]
-    return {board};
-  })();
+const startButton = document.querySelector('#start');
+startButton.addEventListener('click', () => {
+  // gameController.start();
+});
 
-  function Player(name, marker) {
-    return { name, marker };
+// IIFE
+const Gameboard = (()=>{
+  let board = ['','','','','','','','','']
+
+  function render(){
+    board.forEach((square, index)=>{
+      gameboard = document.querySelector('#gameboard');
+      newTile = document.createElement('div');
+      newTile.classList.add("square");
+      newTile.id = `square-${index}`;
+      newTile.textContent = `${square}`;
+      gameboard.appendChild(newTile);
+    })
   }
 
-  const playerOne = Player('one', 'X')
-  const playerTwo = Player('two' ,'O');
+  return {render}
 
-  const displayBoard = (function () {
-    const display = document.querySelector('#board')
-    gameboard.forEach((element) => {
-        let newTile = document.createElement('div')
-        display.appendChild(newTile)
-    });
-    return {};
-  })();
+})();
+
+const gameController = (()=>{
+  let players = [];
+  let curentPlayerIndex;
+  let gameOver = false;
+
+  const start = () => {
+    let one = document.querySelector('#player-one').value
+    let two = document.querySelector('#player-two').value
+    players = [
+      createPlayer(one, 'X'),
+      createPlayer(two, 'O')
+    ]
+
+    curentPlayerIndex = 0;
+  }
+
+})();
+
+const createPlayer = (name, marker) => {
+  return {
+    name,
+    marker
+  }
+}
