107930a1e0ae2e5d471ef45ee063a8e8e904c2b5
Davis <davismickens98@gmail.com>
2024-03-02 09:36:14 -0500

    script,js: added gameboard obj and game controller obj

--- script.js
+++ script.js
@@ -4,9 +4,49 @@ startButton.addEventListener('click', () => {
 });
 const restartButton = document.querySelector('#restart');
 restartButton.addEventListener('click', () => {
-  gameController.rerender();
+  gameController.restart();
 });
+const gameboard = document.querySelector('#gameboard')
 
+const Gameboard = (()=>{
+  let board = ['','','','','','','','','']
+
+  function render(){
+    squareList = document.createElement('div');
+    board.forEach((square, index)=>{
+      square = document.createElement('div');
+      square.classList.add('square');
+      square.id = `${index}`;
+      squareList.appendChild(square)
+    })
+  }
+  gameboard.appendChild(squareList)
+
+  return {
+    render
+  }
+})();
+
+const gameController = (()=>{
+   let players = [];
+   let currentPlayerIndex;
+   let gameOver;
+
+   function start(){
+    players = [
+      createPlayer(document.querySelector('#player-one').value, 'X'),
+      createPlayer(document.querySelector('#player-two').value, 'O')
+    ]
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+
+   }
+
+   return {
+    start
+   }
+})();
 
 // factory
 const createPlayer = (name, marker) => {
